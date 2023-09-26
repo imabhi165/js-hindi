5ed4744ff79508d4df4660da0c65a5b80dd39857
imabhi165 <144894858+imabhi165@users.noreply.github.com>
2023-09-26 19:38:46 +0000

    datatypes

---
+++ basics.js/01_Variables.js
@@ -0,0 +1,23 @@
+const accountId = 14456
+let accountEmail = "abh78909@gmail.com"
+var accountPassword = "12346"
+accountCity = "Bihar"
+
+//accountId = 2  //notallowed
+accountEmail = "ahvhvh@gmail.com"
+accountPassword = "256985"
+accountCity = "Siwan"
+let accountState;
+console.log(accountId);
+
+console.log(accountEmail);
+console.log(accountCity)
+console.log(accountPassword);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
---
+++ basics.js/02_DataType.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using node js thatswhy it will not run 
+//In browser it wil run 
+
+console.log("The sum of two number is : ",8+7)  //code readability should be high
+let name = "Abhishek"
+let age = 18
+let IsLoggedIn = false
+let state;
+console.log(name);
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol = unique
+
+//objects 
+
+console.log(typeof age);
+console.log(typeof name);
+
