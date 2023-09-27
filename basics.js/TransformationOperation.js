86b8b255dcbf434530b14097bb772bdf33e6564f
imabhi165 <144894858+imabhi165@users.noreply.github.com>
2023-09-27 08:05:13 +0000

    transformation

--- basics.js/TransformationOperation.js
+++ basics.js/TransformationOperation.js
@@ -1,9 +1,17 @@
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // **************Operations********
 let value = 3
 let negValue = -value
-console.log(negValue);
+//console.log(negValue);
+
+
+
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
