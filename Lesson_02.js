c37d227ebe8cb11cfa8edadc31a04e82686e31d3
imabhi165 <144894858+imabhi165@users.noreply.github.com>
2023-09-27 08:31:28 +0000

    DataTypeSummary

--- Lesson_02.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello Abhishek")
-console.log(5+5)
-console.log(10*5)
-console.log("This is the thing that i want to learn ")
-console.log("I am going to learn a lot of programming language")
---
+++ basics.js/DataTypesSummary.js
@@ -0,0 +1,33 @@
+// primitive data type 
+
+// 7 types : String, Null , Number , Boolean , Undefined,Symbol, BigInt
+
+
+//Reference DAta type (Non-primitive)
+//Array , Object, Function
+// const heroes = ["Shaktiman","naagRaaj","Doga"]
+// let myObj = {
+//     name = "Abhi";
+//     age = 18;
+// }
+// const score = 100;
+// const scoreValue = 100.3
+// const isLoogedIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+// const bigNumber = 345678955565555
+
+
+
+const MyFunction = function(){
+    console.log("Hello Woerld");
+}
+console.log(typeof MyFunction);
+
+
+
+
