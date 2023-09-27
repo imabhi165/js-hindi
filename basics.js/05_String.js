35316f75805ff7a0c051634e1f6eb9c45892afe4
imabhi165 <144894858+imabhi165@users.noreply.github.com>
2023-09-27 13:03:41 +0000

    string

---
+++ basics.js/05_String.js
@@ -0,0 +1,10 @@
+const name = "Abhi"
+ const repoCount = 10;
+//  console.log(name + repoCount);
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+// `` => This symbol is known as backticks whic is used in modern backend programming language 
+
+// ${} => PlaceHolders 
+ 
+
