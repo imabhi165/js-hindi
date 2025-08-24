6e920fd5f63d2f06657fd32e3a18e208d76a864a
imabhi165 <abhi7808060981@gmail.com>
2025-08-24 23:30:17 +0530

    Dom-Manipulation folder addded

---
+++ Dom-Manipulation/script.js
@@ -0,0 +1,16 @@
+//Dom = Body
+//html se element select krna
+//text badlna
+//html badalna
+//css badalna
+//attributes
+//event listeners
+
+//This is get element by its id.
+// let title = document.getElementById("title");
+// console.dir(title);
+
+//now it will be get elements by its class
+
+let subtitle = document.getElementsByClassName("subtitle");
+console.log(subtitle);
