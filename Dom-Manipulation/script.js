a97021cbeced07f5587476d12521bf1083c7190b
imabhi165 <abhi7808060981@gmail.com>
2025-08-24 23:52:48 +0530

    Added edited Files

--- Dom-Manipulation/script.js
+++ Dom-Manipulation/script.js
@@ -12,5 +12,23 @@
 
 //now it will be get elements by its class
 
-let subtitle = document.getElementsByClassName("subtitle");
-console.log(subtitle);
+// let subtitle = document.getElementsByClassName("subtitle");
+// console.log(subtitle);
+
+// //This is query selector
+// let subtitle2 = document.querySelector("subtitle");
+// console.log(subtitle2);
+
+// //This is query selector all
+// let subtitle3 = document.querySelectorAll("subtitle");
+// console.log(subtitle3);
+
+// //This is get element by its tag name
+// let subtitle4 = document.getElementsByTagName("subtitle");
+// console.log(subtitle4);
+
+let h1 = document.querySelector("h1");
+h1.textContent = "This is the changes done by Abhishek";
+let h1s = document.querySelectorAll("h1");
+console.dir(h1);
+console.dir(h1s);
